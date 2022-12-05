1ad805fa764648f48e9bfc74a8fc35f44847334e
bolotbekovaaidana <aidanabolotbekova42gmail.com>
2022-12-05 15:46:58 +0600

    рр

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Cow cow = new Cow(72, 4, "male", "Tor");
-        Cow cow1 = new Cow(63, 5, "female", "Dasha");
-        Cow cow2 = new Cow(52, 6, "male", "Leo");
-        Cow cow3 = new Cow(42, 7, "female", "Masha");
-        Cow cow4 = new Cow(43, 5, "female", "Sasha");
+        Cow cow = new Cow(172, 4, "male", "Tor");
+        Cow cow1 = new Cow(163, 5, "female", "Dasha");
+        Cow cow2 = new Cow(152, 6, "male", "Leo");
+        Cow cow3 = new Cow(242, 7, "female", "Masha");
+        Cow cow4 = new Cow(128, 5, "female", "Sasha");
         Cow[] cows = {cow, cow1, cow2, cow3, cow4};
         Cow[] cows1 = {cow};
-        Horse horse = new Horse(98, 7, "male", "Tulpar", "black");
-        Horse horse1 = new Horse(78, 4, "female", "Gulpar", "white");
+        Horse horse = new Horse(158, 7, "male", "Tulpar", "black");
+        Horse horse1 = new Horse(128, 4, "female", "Gulpar", "white");
         Horse[] horses = {horse, horse1};
         Horse[] horses1 = {horse1};
         Sheep sheep = new Sheep(54, 4, "female", "Koy");
